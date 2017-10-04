65b31f3fd709fcd42532877c5c44c9c265ad0b19
tiansrealm <tiansrealm@gmail.com>
2017-10-04 02:58:00 -0400

    some progress in sign up. need to do redirect to thanks you page

--- main.go
+++ main.go
@@ -11,15 +11,16 @@ type User struct{
 	name string
 	passw string
 }
+var user_map map[string]User
 
 func login(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		login, err := ioutil.ReadFile("./login.html")
+		page, err := ioutil.ReadFile("./login.html")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Fprintf(w, string(login))
+		fmt.Fprintf(w, string(page))
 	case http.MethodPost:
 		// Parse the form
 		r.ParseForm()
@@ -31,22 +32,41 @@ func login(w http.ResponseWriter, r *http.Request) {
 func sign_up(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		sign_up, err := ioutil.ReadFile("./sign_up.html")
+		page, err := ioutil.ReadFile("./sign_up.html")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Fprintf(w, string(sign_up))
+		fmt.Fprintf(w, string(page))
 	case http.MethodPost:
 		// Parse the form
 		r.ParseForm()
-		// Print the values of the form
-		fmt.Fprintf(w, "post")
+		// Create new user
+		user_map[r.PostFormValue("uname")] = 
+			User{ r.PostFormValue("uname"), r.PostFormValue("psw") }
+
+		fmt.Fprintf(w, "Thanks for signing up")
+		http.Get("google.com")
 	}
 }
-var user_map map[string]User
+
+func thanks_sign_up(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		page, err := ioutil.ReadFile("./thanks_sign_up.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, string(page))
+	case http.MethodPost:
+		
+		http.Get("http://localhost:8080/")
+	}
+}
+
 func main() {
 	user_map = make(map[string]User)
 	http.HandleFunc("/", login)
+	http.HandleFunc("/thanks_sign_up", thanks_sign_up)
 	http.HandleFunc("/sign_up", sign_up)
 	http.ListenAndServe(":8080", nil)
 }
