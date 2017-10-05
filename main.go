1ed63de74f38c719a15eac99962972fc29794cd5
tiansrealm <tiansrealm@gmail.com>
2017-10-05 01:16:22 -0400

    change naming. Made fail to sign up page

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ type User struct{
 	uname string  //username
 	psw string  //password
 }
+
 var user_map map[string]User
+
+var my_user User
+
+var messages map[string]string //key is username, value is message
 
 func login(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -26,19 +31,24 @@ func login(w http.ResponseWriter, r *http.Request) {
 		r.ParseForm()
 		// check if valid user
 		is_valid := false
-		User, is_exist := user_map[r.PostFormValue("uname")]
+		pending_user, is_exist := user_map[r.PostFormValue("uname")]
 
-		if is_exist && User.psw == r.PostFormValue("psw") {
+		if is_exist && pending_user.psw == r.PostFormValue("psw") {
 			is_valid = true
 		}
-		fmt.Fprintf(w, "user validity is %t", is_valid)
+		if is_valid{
+			my_user = pending_user
+			http.Redirect(w, r, "/home", http.StatusTemporaryRedirect)
+		}else{
+			fmt.Fprintf(w, "Your username and password combination is not found")
+		}
 	}
 }
 
 func sign_up(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		page, err := ioutil.ReadFile("./sign_up.html")
+		page, err := ioutil.ReadFile("./sign up.html")
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -46,17 +56,37 @@ func sign_up(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		// Parse the form
 		r.ParseForm()
-		// Create new user
-		user_map[r.PostFormValue("uname")] = 
-			User{ r.PostFormValue("uname"), r.PostFormValue("psw") }
-
-		w.Header().Set("method", "GET")
-		http.Redirect(w, r, "/thanks_sign_up", http.StatusTemporaryRedirect)
+		//check is user exist
+		_, is_exist := user_map[r.PostFormValue("uname")]
+		if is_exist{
+			http.Redirect(w, r, "/fail sign up", http.StatusTemporaryRedirect)
+		}else {
+			// Create new user
+			user_map[r.PostFormValue("uname")] = 
+				User{ r.PostFormValue("uname"), r.PostFormValue("psw") }
+	
+			w.Header().Set("method", "GET")
+			http.Redirect(w, r, "/success sign up", http.StatusTemporaryRedirect)
+		}
 	}
 }
 
-func thanks_sign_up(w http.ResponseWriter, r *http.Request) {
-	page, err := ioutil.ReadFile("./thanks_sign_up.html")
+func success_sign_up(w http.ResponseWriter, r *http.Request) {
+	page, err := ioutil.ReadFile("./success sign up.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, string(page))
+}
+func fail_sign_up(w http.ResponseWriter, r *http.Request) {
+	page, err := ioutil.ReadFile("./fail sign up.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Fprintf(w, string(page))
+}
+func home(w http.ResponseWriter, r *http.Request) {
+	page, err := ioutil.ReadFile("./home.html")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -66,8 +96,10 @@ func thanks_sign_up(w http.ResponseWriter, r *http.Request) {
 func main() {
 	user_map = make(map[string]User)
 	http.HandleFunc("/", login)
-	http.HandleFunc("/thanks_sign_up", thanks_sign_up)
-	http.HandleFunc("/sign_up", sign_up)
+	http.HandleFunc("/sign up", sign_up)
+	http.HandleFunc("/success sign up", success_sign_up)
+	http.HandleFunc("/fail sign up", fail_sign_up)
+	http.HandleFunc("/home", home)
 	http.ListenAndServe(":8080", nil)
 }
 
