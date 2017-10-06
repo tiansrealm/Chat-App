3da95730c60b1aac6618838129b86d7583d5baca
tiansrealm <tiansrealm@gmail.com>
2017-10-05 20:52:19 -0400

    complete part 1. can login, log out, sign up, delete account, and browse messages

--- README.md
+++ README.md
@@ -1,4 +1,30 @@
-# Chat-App
-Chat App using Go lang and http
+# Twitter-App
+By - Tian Lin
 
-Built for Parallel and Distributed System Class
+Written for Parallel and Distributed System Class
+
+
+Twitter like App using Go lang and http
+
+Current Functionalities
+
+Login Page (path = "/")
+-Can log in or direct to sign up page
+-checks if username and password exists and are correct
+-on success will direct to home page
+
+
+Sign Up Page
+-Can sign up new user
+-checks if user already exist and direct to a fail signed up page
+
+
+Home Page
+-can post a message
+-can browse a user's recent messages, including your own by searching username
+-can log out
+-can delete account
+
+
+Browse Page
+-displays result from the search from home page
--- main.go
+++ main.go
@@ -7,16 +7,43 @@ import (
 	"net/http"
 )
 
+//==================================================================================
+
+//success page with a message %s and option to go to login page
+var success_page string = `
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Chat App</title>
+</head>
+
+<body>
+    <h1>%s</h1>
+    <br />
+     <form>
+    <button formaction="/">Return to Log In Page</button>
+	</form>
+</body>
+
+</html>
+`
+
+//===============================================================================
 type User struct {
-	uname    string //username
-	psw      string //password
-	messages []string
+	uname    string   //username
+	psw      string   //password
+	messages []string //twitter posts
 }
 
 var user_map map[string]*User = make(map[string]*User)
 
+//key is username
+
 var my_user *User
 
+//=====================================================================================
 func login(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
@@ -26,7 +53,6 @@ func login(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Fprintf(w, string(page))
 	case http.MethodPost:
-		// Parse the form
 		r.ParseForm()
 		// check if valid user
 		is_valid := false
@@ -47,43 +73,40 @@ func login(w http.ResponseWriter, r *http.Request) {
 func sign_up(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		page, err := ioutil.ReadFile("./sign up.html")
+		page, err := ioutil.ReadFile("./sign_up.html")
 		if err != nil {
 			log.Fatal(err)
 		}
 		fmt.Fprintf(w, string(page))
 	case http.MethodPost:
-		// Parse the form
 		r.ParseForm()
 		//check is user exist
 		_, is_exist := user_map[r.PostFormValue("uname")]
 		if is_exist {
-			http.Redirect(w, r, "/fail sign up", http.StatusTemporaryRedirect)
+			http.Redirect(w, r, "/fail_sign_up", http.StatusTemporaryRedirect)
 		} else {
 			// Create new user
 			user_map[r.PostFormValue("uname")] =
 				&User{r.PostFormValue("uname"), r.PostFormValue("psw"), []string{}}
 
 			w.Header().Set("method", "GET")
-			http.Redirect(w, r, "/success sign up", http.StatusTemporaryRedirect)
+			http.Redirect(w, r, "/success_sign_up", http.StatusTemporaryRedirect)
 		}
 	}
 }
 
 func success_sign_up(w http.ResponseWriter, r *http.Request) {
-	page, err := ioutil.ReadFile("./success sign up.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Fprintf(w, string(page))
+	fmt.Fprintf(w, success_page, "You have sucessfully signed up!")
 }
+
 func fail_sign_up(w http.ResponseWriter, r *http.Request) {
-	page, err := ioutil.ReadFile("./fail sign up.html")
+	page, err := ioutil.ReadFile("./fail_sign_up.html")
 	if err != nil {
 		log.Fatal(err)
 	}
 	fmt.Fprintf(w, string(page))
 }
+
 func home(w http.ResponseWriter, r *http.Request) {
 	page, err := ioutil.ReadFile("./home.html")
 	if err != nil {
@@ -92,29 +115,29 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, string(page))
 
 }
+
 func sucess_new_post(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
 		fmt.Fprintf(w, "Please post from home screen")
 	case http.MethodPost:
-		// Parse the form
 		r.ParseForm()
 		my_user.messages = append(my_user.messages, r.PostFormValue("message"))
 		user_map[my_user.uname].messages = my_user.messages
-		//fmt.Printf("user has %d message\n", len(my_user.messages))
 		fmt.Fprintf(w, "successlly posted new message. Please go back")
 	}
 }
+
 func browse(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		//just print everyone's post
+		//shouldnt be here
 		fmt.Fprintf(w, "please browse from home page")
 	case http.MethodPost:
-		// Parse the form
 		r.ParseForm()
-		searched_user := user_map[r.PostFormValue("uname")]
-		page := `<!DOCTYPE html>
+		searched_user, ok := user_map[r.PostFormValue("uname")]
+		if ok {
+			page := `<!DOCTYPE html>
 				<html lang="en">
 				<head>
 				    <meta charset="UTF-8">
@@ -125,28 +148,63 @@ func browse(w http.ResponseWriter, r *http.Request) {
 				    %s 
 				</body>				
 				</html>`
-		//show last ten messages from the searched user
-		print_data := ""
-		message_array := searched_user.messages
-		//fmt.Printf("There are %d messages\n", len(message_array))
-		if len(message_array) > 10 {
-			message_array = message_array[len(message_array)-10:]
+			//show recent ten messages from the searched user
+			print_data := ""
+			message_array := searched_user.messages
+			if len(message_array) > 10 {
+				message_array = message_array[len(message_array)-10:]
+			}
+			for _, message := range message_array {
+				print_data = print_data + "<p>" + message + "</p>"
+			}
+			fmt.Fprintf(w, page, searched_user.uname, print_data)
+		} else {
+			fmt.Fprintf(w, "No such user exists. Please go back.")
 		}
-		for _, message := range message_array {
-			print_data = print_data + "<p>" + message + "</p>"
-		}
-		fmt.Fprintf(w, page, searched_user.uname, print_data)
+
 	}
 
 }
+
+func delete_account(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		page, err := ioutil.ReadFile("./delete_account.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Fprintf(w, string(page))
+	case http.MethodPost:
+		r.ParseForm()
+		searched_user, ok := user_map[r.PostFormValue("uname")]
+		if ok && searched_user.psw == r.PostFormValue("psw") {
+			//delete user
+			delete(user_map, r.PostFormValue("uname"))
+
+			//option to go back to login screen
+			fmt.Fprintf(w, success_page, "Successfully deleted account.")
+		} else {
+			fmt.Fprintf(w, "Incorrect Username and Password combination")
+		}
+	}
+}
+
+func log_out(w http.ResponseWriter, r *http.Request) {
+	//clears my user and return to login page
+	my_user = nil
+	http.Redirect(w, r, "/", http.StatusTemporaryRedirect)
+}
 func main() {
 	user_map["admin"] = &User{"admin", "admin", []string{}} //default user for faster testing
+	my_user = nil
 	http.HandleFunc("/", login)
-	http.HandleFunc("/sign up", sign_up)
-	http.HandleFunc("/success sign up", success_sign_up)
-	http.HandleFunc("/fail sign up", fail_sign_up)
+	http.HandleFunc("/sign_up", sign_up)
+	http.HandleFunc("/success_sign_up", success_sign_up)
+	http.HandleFunc("/fail_sign_up", fail_sign_up)
 	http.HandleFunc("/home", home)
-	http.HandleFunc("/sucess new post", sucess_new_post)
+	http.HandleFunc("/sucess_new_post", sucess_new_post)
 	http.HandleFunc("/browse", browse)
+	http.HandleFunc("/log_out", log_out)
+	http.HandleFunc("/delete_account", delete_account)
 	http.ListenAndServe(":8080", nil)
 }
