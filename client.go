d50ce5ecf29e2ad82d9525750e6fbc1fee015037
tiansrealm <tiansrealm@gmail.com>
2017-11-02 01:09:45 -0400

    debugging query system

---
+++ client.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+)
+
+const END_TAG = "<end>"
+
+func main() {
+	conn, _ := net.Dial("tcp", "localhost:8083")
+	defer conn.Close()
+	scanner := bufio.NewScanner(os.Stdin)
+	scannerConn := bufio.NewScanner(conn)
+	for {
+		query := ""
+		for scanner.Scan() {
+			newLine := scanner.Text()
+			query += newLine //append to read query
+			index_of_endtag := strings.Index(newLine, END_TAG)
+			if index_of_endtag != -1 {
+				//end of query
+				break
+			}
+		}
+		//send query
+		fmt.Fprintf(conn, query+"\n")
+
+		response := ""
+		for scannerConn.Scan() {
+			newLine := scannerConn.Text()
+			response += newLine //append to read query
+			index_of_endtag := strings.Index(newLine, END_TAG)
+			if index_of_endtag != -1 {
+				//reached end of response
+				break
+			}
+		}
+		response = strings.Replace(response, END_TAG, "", -1) //remove tag
+		fmt.Println(response)
+	}
+}
--- server.go
+++ server.go
@@ -24,7 +24,12 @@ type User struct {
 var user_map map[string]*User = make(map[string]*User)
 
 const USERLIST_FILENAME = "user_list.txt"
-const MESSAGE_ENDTAG = "<end of message>"
+
+//used to sperate user messages in files
+const USER_MESSAGE_DELIMITER = "<end of message>"
+
+//used to signify end of a communication message
+const END_TAG = "<end>"
 
 func check(e error) {
 	if e != nil {
@@ -35,6 +40,8 @@ func check(e error) {
 //===================================================================
 func main() {
 	/*main function initialize server socket and listens for queries*/
+
+	//load user list for faster access to a list of current users
 	load_user_list()
 	in, _ := net.Listen("tcp", ":8083")
 	defer in.Close()
@@ -43,70 +50,30 @@ func main() {
 		conn, _ := in.Accept()
 		fmt.Fprintln(os.Stderr, "Got a connection")
 		scanner := bufio.NewScanner(conn)
+		query := ""
 		for scanner.Scan() {
-			query := scanner.Text()
-			fmt.Println("got " + query)
-			evaluate(query, conn)
-
+			fmt.Println("-------------------------------------------")
+			newLine := scanner.Text()
+			index_of_endtag := strings.Index(newLine, END_TAG)
+			if index_of_endtag != -1 {
+				//reached end
+				query += newLine[0 : len(newLine)-len(END_TAG)] //append without end tag
+				fmt.Println(query)
+				finished_query := query
+				query = "" //reset query
+				evaluate(finished_query, conn)
+				break
+			} else {
+				query += newLine //append to read query
+			}
 		}
 		//interupted
-		fmt.Fprintln(os.Stderr, "connection gone!")
+		fmt.Fprintln(os.Stderr, "connection gone!") //print ln have \n
 		conn.Close()
 	}
 
 }
-func evaluate(query string, conn net.Conn) {
-	/*
-		handles queries
-		separate string into args by spliting along delimiter ":"
-		Pre-Condition:
-		Request will be in the form of "queryname: arg1: arg2: ..."
-		Post Condition:
-		response with be in the form of  "success/Error: value"
-	*/
-	delimiter := ":"
-	parsed_query := strings.Split(query, delimiter)
-	//trims white space
-	for i := 0; i < len(parsed_query); i++ {
-		parsed_query[i] = strings.TrimSpace(parsed_query[i])
-	}
-	switch parsed_query[0] {
-	case "does user exist":
-		//args should be query, username
-		if len(parsed_query) != 2 {
-			fmt.Fprintf(conn, "Error: Not valid # of args\n")
-			return
-		}
-		_, is_exist := user_map[parsed_query[1]]
-		if is_exist {
-			fmt.Fprintf(conn, "success: true\n")
-		} else {
-			fmt.Fprintf(conn, "success: false\n")
-		}
-	case "add user":
-		//args should be query, username, password
-		if len(parsed_query) != 3 {
-			fmt.Fprintf(conn, "Error: Not valid # of args\n")
-			return
-		}
-		add_user(parsed_query[1], parsed_query[2])
-		fmt.Fprintf(conn, "success: I added user %s\n", parsed_query[1])
-	case "add message":
-		//args should be query, username, message
-		if len(parsed_query) != 3 {
-			fmt.Fprintf(conn, "Error: Not valid # of args\n")
-			return
-		}
-		add_message(parsed_query[1], parsed_query[2])
-		fmt.Fprintf(conn, "success: added message for%s\n", parsed_query[1])
-	default:
-		fmt.Fprintf(conn, "Error:%s is not a valid query\n", parsed_query[0])
 
-	}
-
-}
-
-//==========================================================================================
 func load_user_list() {
 	/*loads list of existing users from file database into server memory*/
 	file, err := os.OpenFile(USERLIST_FILENAME, os.O_CREATE|os.O_RDONLY, 0600)
@@ -124,78 +91,124 @@ func load_user_list() {
 
 }
 
-func add_user(uname string, psw string) {
+func evaluate(query string, conn net.Conn) {
+	/*
+		handles queries
+		separate string into args by spliting along delimiter ":"
+		Pre-Condition:
+		Request will be in the form of "queryname: arg1: arg2: ..."
+		Post Condition:
+		response with be in the form of  "success/error: response"
+	*/
+	delimiter := ":"
+	parsed_query := strings.Split(query, delimiter)
+	//trims white space
+	for i := 0; i < len(parsed_query); i++ {
+		parsed_query[i] = strings.TrimSpace(parsed_query[i])
+	}
+	switch parsed_query[0] {
+	case "does user exist":
+		//args should be query, username
+		check_args(parsed_query, 2, conn)
+		_, is_exist := user_map[parsed_query[1]]
+		if is_exist {
+			fmt.Fprintf(conn, "success: true %s \r\n", END_TAG)
+		} else {
+			fmt.Fprintf(conn, "success: true %s \r\n", END_TAG)
+		}
+	case "add user":
+		//args should be query, username, password
+		check_args(parsed_query, 3, conn)
+		add_user(parsed_query[1], parsed_query[2], conn)
+	case "add message":
+		//args should be query, username, message
+		check_args(parsed_query, 3, conn)
+		add_message(parsed_query[1], parsed_query[2], conn)
+	case "read messages":
+	default:
+		fmt.Fprintln(os.Stderr, "I sent error\n")
+		fmt.Fprintf(conn, "error: %s is not a valid query.%s\n", parsed_query[0], END_TAG)
+
+	}
+
+}
+
+//==========================================================================================
+//Functions that respond to queries, used in Evalute
+//
+func check_args(parsed_query []string, num_expected int, conn net.Conn) {
+	if len(parsed_query) != num_expected {
+		fmt.Fprintf(conn, "error: Not valid # of args%s\n", END_TAG)
+		return
+	}
+}
+func add_user(uname string, psw string, conn net.Conn) {
 	/*Create new user and write new user info to user list file*/
 
 	_, is_exist := user_map[uname]
 	if !is_exist {
 		//create user if not exist
-		user_map[uname] = &User{uname, psw, []string{}}
+		user_map[uname] = &User{uname, psw}
+		//open user list file to write to end of it
+		file, err := os.OpenFile(USERLIST_FILENAME, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+		check(err)
+		defer file.Close()
+		text := uname + " : " + psw + "\r\n"
+		if _, err = file.WriteString(text); err != nil {
+			panic(err)
+		}
+		//create user data file
+		u_file_name := uname + ".txt"
+		os.Remove(u_file_name) // clear old junk
+		created_file, err := os.Create(u_file_name)
+		check(err)
+		defer created_file.Close()
+		//response
+		fmt.Fprintf(conn, "success: I added user %s.%s\n", uname, END_TAG)
+	} else {
+		//negative response
+		fmt.Fprintf(conn, "error: user, %s, already exists.%s\n", uname, END_TAG)
 	}
-	//open user list file to write to end of it
-	file, err := os.OpenFile(USERLIST_FILENAME, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	check(err)
-	defer file.Close()
-	text := uname + " : " + psw + "\r\n"
-	if _, err = file.WriteString(text); err != nil {
-		panic(err)
-	}
-	//create user data file
-	u_file_name := uname + ".txt"
-	os.Remove(u_file_name) // clear old junk
-	created_file, err := os.Create(u_file_name)
-	check(err)
-	defer created_file.Close()
 }
 
-func add_message(uname string, new_message string) {
+func add_message(uname string, new_message string, conn net.Conn) {
 	/*
 		Add a new message under the user with given uname, by
 		writing to database file containing stored messsages the user
 	*/
-	user := user_map[uname]
 	filename := uname + ".txt"
 	message_file, err := os.OpenFile(filename, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
 	check(err)
 	defer message_file.Close()
-	// if user.messages == nil {
-	// 	//load messages for this user since haven't already done so
-	// 	reader := bufio.NewReader(file)
-	// 	//read until the delimiter for end of a message
-	// 	for stored_message, err := reader.ReadString(MESSAGE_ENDTAG) {
-	// 		if err == io.EOF{
-	// 			break
-	// 		}else {check(err)}
-	// 		stored_message := stored_message[0:len(stored_message)-len(MESSAGE_ENDTAG)]
-	// 		// store user message in server memory
-	// 		user.messages = append(user.messages, stored_message)
-	// 		user_map[user.uname].messages = user.messages
-
-	// 	}
-	// }
 
 	//write new message to file
-	text_to_write := new_message + MESSAGE_ENDTAG
-	if _, err = file.WriteString(text_to_write); err != nil {
+	text_to_write := new_message + USER_MESSAGE_DELIMITER
+	if _, err = message_file.WriteString(text_to_write); err != nil {
+		fmt.Fprintf(conn, "error: server failed to write\n")
 		panic(err)
+	} else {
+		fmt.Fprintf(conn, "success: added message for%s\n", uname)
 	}
 
 }
 
-func read_messages(uname string, num_messages int) {
+func read_messages(uname string, num_messages int, conn net.Conn) {
 	/*reads messages from user file database*/
+
+	filename := uname + ".txt"
+	message_file, err := os.OpenFile(filename, os.O_CREATE|os.O_RDONLY, 0600)
 	check(err)
 	defer message_file.Close()
 
 	messages_in_byte, err := ioutil.ReadFile(filename)
 	if err != nil {
-		Panic(err)
+		panic(err)
 	}
 	messages_in_string := string(messages_in_byte)
 
-	message_array := strings.Split(messages_in_string, MESSAGE_ENDTAG)
+	message_array := strings.Split(messages_in_string, USER_MESSAGE_DELIMITER)
 
 	recent_messages := message_array[len(message_array)-num_messages:]
-
+	fmt.Fprintf(os.Stderr, "printing message%s\n", recent_messages)
 	//give back most recent num_messages of messages
 }
